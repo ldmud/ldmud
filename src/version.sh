3104b345f5f978026c1ef8b7ea487c1ec2684360
Lars <lars@bearnip.com>
2005-08-24 03:41:51 +0000

    The PRCS rule in the Makefile now first checks if PRCS is available. Clarified the MOVE_OBJECT hooks.
    git-svn-id: svn://svn.bearnip.com/ldmud/trunk@23 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/version.sh
+++ src/version.sh
@@ -3,20 +3,20 @@
 
 # The checkin date
 # $Format: "version_date=\"$ProjectDate$\""$ 
-version_date=$(echo "\$Date$" | sed -e 's/\$Date: \([0-9]*-[0-9]*-[0-9]*\) \([0-9]*:[0-9]*:[0-9]*\).*\$/\1 \2/')
+version_date="Mon, 20 Jun 2005 19:42:30 -0600"
 
 # The checkin revision
-# $Format: "version_revision=\"$ProjectVersion$\""$ 
-version_revision=$(echo "\$Revision$" | sed -e 's/\$Revision: \([0-9][0-9]*\) *\$/\1/')
+# $Format: "version_revision=$ProjectVersion$"$ 
+version_revision=3-3.710
 
 # The version type: dev, stable, maint release
 # $Format: "version_type=\"$ReleaseType$\""$ 
 version_type="dev"
 
 # The version number information
-# $Format: "version_micro=\"$ReleaseMicro$\""$ 
+# $Format: "version_micro=$ReleaseMicro$"$ 
 version_micro=710
-# $Format: "version_minor=\"$ReleaseMinor$\""$ 
+# $Format: "version_minor=$ReleaseMinor$"$ 
 version_minor=3
-# $Format: "version_major=\"$ReleaseMajor$\""$ 
+# $Format: "version_major=$ReleaseMajor$"$ 
 version_major=3
