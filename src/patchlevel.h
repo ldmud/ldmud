b767601970cedcefedbab33395941bbb489d241c
Lars <lars@bearnip.com>
2005-10-27 04:54:49 +0000

    Load 3.3.516 into branch/prcs-import-33.
    git-svn-id: svn://svn.bearnip.com/ldmud/branch/prcs-import-33@1101 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -18,10 +18,10 @@
 #define IS_RELEASE() (!strcmp("dev", "rel"))
 
 /* $Format: "#define RELEASE_DATE \"$ProjectDate$\""$ */
-#define RELEASE_DATE "Mon, 12 Apr 2004 22:24:42 -0600"
+#define RELEASE_DATE "Tue, 13 Apr 2004 18:31:53 -0600"
 
 /* $Format: "#define PROJ_VERSION \"$ProjectVersion$\""$ */
-#define PROJ_VERSION "3-3.515"
+#define PROJ_VERSION "3-3.516"
 
 #if IS_STABLE
 
@@ -29,15 +29,15 @@
 #define GAME_VERSION "3.3.0"
 
 /* $Format: "#define LONG_VERSION \"$ReleaseVersion$-$ReleaseType$.$ProjectMinorVersion$\""$ */
-#define LONG_VERSION "3.3.0-dev.515"
+#define LONG_VERSION "3.3.0-dev.516"
 
 #else
 
 /* $Format: "#define GAME_VERSION \"$DevelopmentVersion$\""$ */
-#define GAME_VERSION "3.3.515"
+#define GAME_VERSION "3.3.516"
 
 /* $Format: "#define LONG_VERSION \"$DevelopmentVersion$\""$ */
-#define LONG_VERSION "3.3.515"
+#define LONG_VERSION "3.3.516"
 
 #endif
 
@@ -51,7 +51,7 @@
 #define VERSION_MICRO "0"
 
 /* $Format: "#define VERSION_PATCH \"$ProjectMinorVersion$\""$ */
-#define VERSION_PATCH "515"
+#define VERSION_PATCH "516"
 
 /* $Format: "#define LOCAL_LEVEL \"\""$ */
 #define LOCAL_LEVEL ""
