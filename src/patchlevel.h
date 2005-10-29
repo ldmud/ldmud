a7f6843f0d406504d02d6a4ad16df681aba10d58
Lars <lars@bearnip.com>
2005-10-29 05:04:57 +0000

    Load 3.3.709 into branch/prcs-import-33.
    git-svn-id: svn://svn.bearnip.com/ldmud/branch/prcs-import-33@1493 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -22,13 +22,13 @@
 #define RELEASE_TYPE "dev"
 
 /* $Format: "#define RELEASE_DATE \"$ProjectDate$\""$ */
-#define RELEASE_DATE "Sun, 19 Jun 2005 23:09:27 -0600"
+#define RELEASE_DATE "Mon, 20 Jun 2005 08:13:01 -0600"
 
 /* $Format: "#define PROJ_VERSION \"$ProjectVersion$\""$ */
-#define PROJ_VERSION "3-3.708"
+#define PROJ_VERSION "3-3.709"
 
 /* $Format: "#define DRIVER_VERSION \"$ReleaseVersion$\""$ */
-#define DRIVER_VERSION "3.3.708"
+#define DRIVER_VERSION "3.3.709"
 
 /* $Format: "#define VERSION_MAJOR \"$ReleaseMajor$\""$ */
 #define VERSION_MAJOR "3"
@@ -37,7 +37,7 @@
 #define VERSION_MINOR "3"
 
 /* $Format: "#define VERSION_MICRO \"$ReleaseMicro$\""$ */
-#define VERSION_MICRO "708"
+#define VERSION_MICRO "709"
 
 /* $Format: "#define LOCAL_LEVEL \"\""$ */
 #define LOCAL_LEVEL ""
--- src/xalloc.c
+++ src/xalloc.c
@@ -148,7 +148,7 @@ static t_stat xalloc_stat = {0,0};
   /* Total number and size of allocations done by the driver (incl overhead).
    */
 
-#if defined(REPLACE_MALLOC) && defined(SBRK_OK)
+#if defined(SBRK_OK)
 static t_stat clib_alloc_stat = {0,0};
   /* Number and size of allocations done through the clib emulation
    * functions (incl overhead).
