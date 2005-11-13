fd9b8a351383e77e9934cdb554de68a1547d76f1
Lars <lars@bearnip.com>
2005-11-13 05:18:39 +0000

    Tag branch/prcs-import-32-1 as version/3.2-dev/36.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1603 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 36/access_check.h
@@ -0,0 +1,17 @@
+#ifndef __ACCESS_CHECK_H__
+#define __ACCESS_CHECK_H__ 1
+
+#include "config.h"
+
+#if defined(ACCESS_CONTROL)
+
+#include "driver.h"
+#include "comm.h" /* TODO: struct sockaddr_in */
+
+extern char * allow_host_access(struct sockaddr_in *full_addr, long *idp);
+extern void release_host_access(long num);
+
+#endif /* ACCESS_CONTROL */
+
+#endif /* __ACCESS_CHECK_H__ */
+
