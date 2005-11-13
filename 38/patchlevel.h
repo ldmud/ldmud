c7fbb3d3b93caef95a372e0dd50871336d1dadcd
Lars <lars@bearnip.com>
2005-11-13 05:24:50 +0000

    Tag branch/prcs-import-32-1 as version/3.2-dev/38.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1608 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 38/patchlevel.h
@@ -0,0 +1,27 @@
+#ifndef __PATCHLEVEL_H__
+#define __PATCHLEVEL_H__ 1
+
+/* $Format: "#define RELEASE_DATE \"$ProjectDate$\""$ */
+#define RELEASE_DATE "Wed, 17 Feb 1999 23:46:49 +0000"
+
+/* $Format: "#define PROJ_VERSION \"$ProjectVersion$\""$ */
+#define PROJ_VERSION "3-2-dev.38"
+
+/* $Format: "#define GAME_VERSION \"$ReleaseVersion$\""$ */
+#define GAME_VERSION "3.2.6"
+
+/* $Format: "#define LONG_VERSION \"$ReleaseVersion$-$ReleaseType$.$ProjectMinorVersion$\""$ */
+#define LONG_VERSION "3.2.6-dev.38"
+
+/* $Format: "#define LOCAL_LEVEL \"\""$ */
+#define LOCAL_LEVEL ""
+
+/* TODO: Add something like the perl local patchlevel management. */
+/* TODO: Add LPC defines for the various version number parts */
+/* TODO: Make the necessary mods for PRCS */
+
+/* $Format: "#define IS_RELEASE() (!strcmp(\"$ReleaseType$\", \"rel\"))"$ */
+#define IS_RELEASE() (!strcmp("dev", "rel"))
+
+#endif /* __PATCHLEVEL_H__ */
+
