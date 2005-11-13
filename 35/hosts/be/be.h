6d3e0c32ee5629a1645d7271913a32fb7f67314a
Lars <lars@bearnip.com>
2005-11-13 05:15:18 +0000

    Tag branch/prcs-import-32-1 as version/3.2-dev/35.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1600 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 35/hosts/be/be.h
@@ -0,0 +1,23 @@
+#ifndef HOSTS_BE_H
+#define HOSTS_BE_H
+
+#define SOCKET_HEADER "hosts/be/socketinc.h"
+#define SOCKET_LIB 1
+
+#undef HOST_DEPENDENT_INIT
+#define HOST_DEPENDENT_INIT \
+    { no_erq_demon++; /* Be's fork() does not cover sockets, thus no auto-erq */\
+    }
+
+
+#ifdef __MWERKS__
+#define isascii(c) !((unsigned long)c & 0xFFFFFF80)
+#define EMSGSIZE 40
+#endif
+
+#define FNDELAY O_NONBLOCK
+#ifndef CHARBITS
+#  define CHARBITS (0xFF)
+#endif
+
+#endif
