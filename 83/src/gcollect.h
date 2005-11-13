253a728d47e843a8bf61c3e4faa9562f1c7b5c5a
Lars <lars@bearnip.com>
2005-11-13 09:34:37 +0000

    Tag branch/prcs-import-32-1 as version/3.2-dev/83.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1720 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 83/src/gcollect.h
@@ -0,0 +1,43 @@
+#ifndef __GCOLLECT_H__
+#define __GCOLLECT_H__ 1
+
+#include "driver.h"
+
+#include "exec.h"       /* struct program */
+#include "interpret.h"  /* struct svalue */
+#include "object.h"     /* struct object */
+
+#if defined(MALLOC_smalloc)
+
+/* --- Variables --- */
+
+extern int gcollect_outfd;
+extern int garbage_collection_in_progress;
+extern struct object *gc_obj_list_destructed;
+extern struct lambda *stale_misc_closures;
+extern struct mapping *stale_mappings;
+
+
+/* --- Prototypes --- */
+
+extern void clear_memory_reference(char *p);
+extern void clear_inherit_ref(struct program *p);
+extern void mark_program_ref(struct program *p);
+extern void reference_destructed_object(struct object *ob);
+extern void note_malloced_block_ref(char *p);
+extern void count_ref_from_string(char *p);
+extern void count_ref_in_vector(struct svalue *svp, int num);
+extern void clear_ref_in_vector(struct svalue *svp, int num);
+
+#endif /* MALLOC_smalloc */
+
+/* --- Variables --- */
+
+extern time_t time_last_gc;
+
+/* --- Prototypes --- */
+
+extern void garbage_collection(void);
+extern void setup_print_block_dispatcher(void);
+
+#endif /* __GCOLLECT_H__ */
