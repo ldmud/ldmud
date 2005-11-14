8afb10f27d560e2693b27d7e7932a65c71401a8d
Lars <lars@bearnip.com>
2005-11-14 02:46:20 +0000

    Tag branch/prcs-import-32-444 as version/3.2-dev/460.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1806 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 460/mud/lp-245/room/plane7.c
@@ -0,0 +1,9 @@
+#include "room.h"
+
+FOUR_EXIT("room/plane5", "south",
+	  "room/plane10", "north",
+	  "room/plane3", "east",
+	  "room/big_tree", "west",
+	  "A large open plain",
+	  "A large open plain. There is a big tree to the west.\n",
+	  1)
