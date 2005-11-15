a80aeb8bed1e9bd5bbdf0ccde7096cf6af42d62d
Lars <lars@bearnip.com>
2005-11-15 05:17:00 +0000

    Tag branch/prcs-import-32-444 as version/3.2-dev/490.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1869 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 490/mud/lp-245/obj/weapon.c
@@ -0,0 +1,174 @@
+/*
+ * This file defines a general purpose weapon. See below for configuration
+ * functions: set_xx.
+ */
+
+/*
+ * If you are going to copy this file, in the purpose of changing
+ * it a little to your own need, beware:
+ *
+ * First try one of the following:
+ *
+ * 1. Do clone_object(), and then configure it. This object is specially
+ *    prepared for configuration.
+ *
+ * 2. If you still is not pleased with that, create a new empty
+ *    object, and make an inheritance of this objet on the first line.
+ *    This will automatically copy all variables and functions from the
+ *    original object. Then, add the functions you want to change. The
+ *    original function can still be accessed with '::' prepended on the name.
+ *
+ * The maintainer of this LPmud might become sad with you if you fail
+ * to do any of the above. Ask other wizards if you are doubtful.
+ *
+ * The reason of this, is that the above saves a lot of memory.
+ */
+
+status wielded;
+string wielded_by;
+string name_of_weapon;
+string cap_name;
+string alt_name;
+string alias_name;
+string short_desc;
+string long_desc;
+string read_msg;
+int class_of_weapon;
+int value;
+int local_weight;
+object hit_func;
+object wield_func;
+string info;
+
+query_name() { return name_of_weapon; }
+
+long() {
+    write(long_desc);
+}
+
+reset(arg) {
+    if (arg)
+	return;
+    wielded = 0; value = 0;
+}
+
+init() {
+    if (read_msg) {
+	add_action("read", "read");
+    }
+    add_action("wield", "wield");
+}
+
+id(str) {
+    return str == name_of_weapon || str == alt_name || str == alias_name;
+}
+
+wield(str) {
+    if (!id(str))
+	return 0;
+    if (environment() != this_player()) {
+	/* write("You must get it first!\n"); */
+	return 0;
+    }
+    if (wielded) {
+	write("You already wield it!\n");
+	return 1;
+    }
+    if(wield_func)
+	if(!wield_func->wield(this_object()))
+	    return 1;
+    wielded_by = this_player();
+    this_player()->wield(this_object());
+    wielded = 1;
+    return 1;
+}
+
+short() {
+    if (wielded)
+	if(short_desc)
+	    return short_desc + " (wielded)";
+    return short_desc;
+}
+
+weapon_class() {
+    return class_of_weapon;
+}
+
+drop(silently) {
+    if (wielded) {
+	wielded_by->stop_wielding();
+	wielded = 0;
+	if (!silently)
+	    write("You drop your wielded weapon.\n");
+    }
+    return 0;
+}
+
+un_wield() {
+    if (wielded)
+	wielded = 0;
+}
+
+hit(attacker)
+{
+    if (hit_func)
+	return hit_func->weapon_hit(attacker);
+    return 0;
+}
+
+set_id(n) {
+    name_of_weapon = n;
+    cap_name = capitalize(n);
+    short_desc = cap_name;
+    long_desc = "You see nothing special.\n";
+}
+
+set_name(n) {
+    name_of_weapon = n;
+    cap_name = capitalize(n);
+    short_desc = cap_name;
+    long_desc = "You see nothing special.\n";
+}
+
+read(str) {
+    if (!id(str))
+	return 0;
+    write(read_msg);
+    return 1;
+}
+
+query_value() {
+    return value;
+}
+
+get() { return 1; }
+
+query_weight() { return local_weight; }
+
+set_class(c) { class_of_weapon = c; }
+
+set_weight(w) { local_weight = w; }
+
+set_value(v) { value = v; }
+
+set_alt_name(n) { alt_name = n; }
+
+set_hit_func(ob) { hit_func = ob; }
+
+set_wield_func(ob) { wield_func = ob; }
+
+set_alias(n) { alias_name = n; }
+
+set_short(sh) { short_desc = sh; long_desc = short_desc + "\n";}
+
+set_long(long) { long_desc = long; }
+
+set_read(str) { read_msg = str; }
+
+set_info(i) {
+    info = i;
+}
+
+query_info() {
+    return info;
+}
---
+++ 490/src/typedefs.h
@@ -0,0 +1,43 @@
+#ifndef TYPEDEFS_H__
+#define TYPEDEFS_H__ 1
+
+/*---------------------------------------------------------------------------
+ * Type definitions for opaque uses.
+ *
+ *---------------------------------------------------------------------------
+ * This file defines several types whose opaque definitions need to be known
+ * in many places. Main purpose is to decouple the header files.
+ */
+
+#include "driver.h"
+
+typedef struct action_s           action_t;           /* sent.h */
+typedef struct callback_s         callback_t;         /* simulate.h */
+typedef struct case_list_entry_s  case_list_entry_t;  /* switch.h */
+typedef struct case_state_s       case_state_t;       /* switch.h */
+typedef struct ed_buffer_s        ed_buffer_t;        /* ed.c */
+typedef struct function_s         function_t;         /* exec.h */
+typedef struct ident_s            ident_t;            /* lex.h */
+typedef struct include_s          include_t;          /* exec.h */
+typedef struct inherit_s          inherit_t;          /* exec.h */
+typedef struct interactive_s      interactive_t;      /* comm.h */
+typedef struct input_to_s         input_to_t;         /* comm.h */
+typedef struct instr_s            instr_t;            /* exec.h */
+typedef struct lambda_s           lambda_t;           /* closure.h */
+typedef struct linenumbers_s      linenumbers_t;      /* exec.h */
+typedef struct mapping_s          mapping_t;          /* mapping.h */
+typedef struct object_s           object_t;           /* object.h */
+typedef struct program_s          program_t;          /* exec.h */
+typedef struct replace_ob_s       replace_ob_t;       /* object.h */
+typedef struct rt_context_s       rt_context_t;       /* backend.h */
+typedef struct sentence_s         sentence_t;         /* sent.h */
+typedef struct shadow_s           shadow_t;           /* sent.h */
+typedef struct simul_efun_table_s simul_efun_table_t; /* simul_efun.h */
+typedef unsigned long             StrRefCount;        /* stralloc */
+typedef struct strbuf_s           strbuf_t;           /* strfuns.h */
+typedef struct svalue_s           svalue_t;           /* svalue.h */
+typedef struct variable_s         variable_t;         /* exec.h */
+typedef struct vector_s           vector_t;           /* array.h */
+typedef struct wiz_list_s         wiz_list_t;         /* wiz_list.h */
+
+#endif /* TYPEDEFS_H__ */
