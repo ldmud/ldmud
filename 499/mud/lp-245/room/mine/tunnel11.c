2b69c553ba0a5991048b4736cf0f092e6d8e52d4
Lars <lars@bearnip.com>
2005-11-15 05:37:08 +0000

    Tag branch/prcs-import-32-444 as version/3.2-dev/499.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1887 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 499/mud/lp-245/room/mine/tunnel11.c
@@ -0,0 +1,5 @@
+#include "../room.h"
+TWO_EXIT("room/mine/tunnel10", "east",
+	 "room/mine/tunnel12", "north",
+	 "Tunnel",
+	 "In the tunnel into the mines.\n", 0)
---
+++ 499/mud/lp-245/room/mine/tunnel15.c
@@ -0,0 +1,5 @@
+#include "../room.h"
+TWO_EXIT("room/mine/tunnel16", "east",
+	 "room/mine/tunnel14", "west",
+	 "Tunnel",
+	 "In the tunnel into the mines.\n", 0)
