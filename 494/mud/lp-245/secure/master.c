8393fa50ccb18961ce47f4c5ec9cd297eb01e0e9
Lars <lars@bearnip.com>
2005-11-15 05:25:54 +0000

    Tag branch/prcs-import-32-444 as version/3.2-dev/494.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1877 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 494/mud/lp-245/secure/master.c
@@ -0,0 +1,7 @@
+/*  secure/master.c     (plain default)
+**
+** Since LP-245 is a compat mudlib, this master file is just a wrapper
+** around the real master, /obj/master.c
+*/
+
+#include "/obj/master.c"
