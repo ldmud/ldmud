78b56cef39ccf6a60864cea4f37f59549341315a
Lars <lars@bearnip.com>
2005-11-16 03:28:45 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/610.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1987 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 610/mud/lp-245/room/station.c
@@ -0,0 +1,6 @@
+#include "room.h"
+
+ONE_EXIT("room/vill_road2", "up",
+	 "Central point",
+"This is the central point. A lot of traffic seems to have passed through\n"+
+"here. If you just wait long enough, some transport might pick you up.\n", 1)
