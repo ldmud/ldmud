3d806d00928830b38369855d5830a84b82a8201b
Lars <lars@bearnip.com>
2005-11-16 00:45:23 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/592.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1951 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 592/mud/lp-245/obj/living.h
@@ -0,0 +1,9 @@
+#define INTERVAL_BETWEEN_HEALING	10
+#define WEAPON_CLASS_OF_HANDS		(3)
+#define ARMOUR_CLASS_OF_BARE		0
+#define KILL_NEUTRAL_ALIGNMENT		10
+#define ADJ_ALIGNMENT(al)		((-al - KILL_NEUTRAL_ALIGNMENT)/4)
+#define MAX_LIST			20
+#define NAME_OF_GHOST			"some mist"
+
+inherit "obj/living";
---
+++ 592/src/patchlevel.h
@@ -0,0 +1,47 @@
+#ifndef PATCHLEVEL_H__
+#define PATCHLEVEL_H__ 1
+
+/*--------------------------------------------------------------------------
+ * Various version numbers and strings, collected here so that PRCS has
+ * to change only one file.
+ *--------------------------------------------------------------------------
+ */
+
+/* $Format: "#define RELEASE_DATE \"$ProjectDate$\""$ */
+#define RELEASE_DATE "Fri, 18 Jul 2003 01:14:42 -0600"
+
+/* $Format: "#define PROJ_VERSION \"$ProjectVersion$\""$ */
+#define PROJ_VERSION "3-2-dev.592"
+
+/* $Format: "#define GAME_VERSION \"$ReleaseVersion$\""$ */
+#define GAME_VERSION "3.2.10"
+
+/* $Format: "#define LONG_VERSION \"$ReleaseVersion$-$ReleaseType$.$ProjectMinorVersion$\""$ */
+#define LONG_VERSION "3.2.10-dev.592"
+
+/* $Format: "#define VERSION_MAJOR \"$ReleaseMajor$\""$ */
+#define VERSION_MAJOR "3"
+
+/* $Format: "#define VERSION_MINOR \"$ReleaseMinor$\""$ */
+#define VERSION_MINOR "2"
+
+/* $Format: "#define VERSION_MICRO \"$ReleaseMicro$\""$ */
+#define VERSION_MICRO "10"
+
+/* $Format: "#define VERSION_PATCH \"$ProjectMinorVersion$\""$ */
+#define VERSION_PATCH "592"
+
+/* $Format: "#define LOCAL_LEVEL \"\""$ */
+#define LOCAL_LEVEL ""
+
+/* $Format: "#define RELEASE_TYPE \"$ReleaseType$\""$ */
+#define RELEASE_TYPE "dev"
+
+/* TODO: Add something like the perl local patchlevel management. */
+/* TODO: Add LPC defines for the various version number parts */
+
+/* $Format: "#define IS_RELEASE() (!strcmp(\"$ReleaseType$\", \"rel\"))"$ */
+#define IS_RELEASE() (!strcmp("dev", "rel"))
+
+#endif /* PATCHLEVEL_H__ */
+
