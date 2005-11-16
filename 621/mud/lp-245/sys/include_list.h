efcae8b9fbfef94adbf82fc1866d77853adcb770
Lars <lars@bearnip.com>
2005-11-16 04:18:05 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/621.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@2009 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 621/mud/lp-245/sys/include_list.h
@@ -0,0 +1,9 @@
+#ifndef LPC_INCLUDE_LIST_H
+#define LPC_INCLUDE_LIST_H
+
+/* Return value flag types for include_list() */
+
+#define INCLIST_FLAT         0x00
+#define INCLIST_TREE         0x01
+
+#endif /* LPC_INCLUDE_LIST_H */
