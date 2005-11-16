f50698492dd57a758b700abc1f9f6fb1e53db59a
Lars <lars@bearnip.com>
2005-11-16 13:59:42 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/640.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@2050 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 640/mud/lp-245/sys/files.h
@@ -0,0 +1,25 @@
+#ifndef LPC_FILES_H_
+#define LPC_FILES_H_ 1
+
+/* Definitions and macros for the various file efuns */
+
+/* Special return values of file_size() and the size part of get_dir().
+ */
+
+#define FSIZE_NOFILE  -1  /* File doesn't exist or is not readable */
+#define FSIZE_DIR     -2  /* The file is actually a directory */
+
+
+/* Flag values for get_dir().
+ */
+
+#define GETDIR_EMPTY      0  /* return an empty array (not very useful) */
+#define GETDIR_NAMES      1  /* return the filenames */
+#define GETDIR_SIZES      2  /* return the file sizes */
+#define GETDIR_DATES      4  /* return the dates of last modification */
+#define GETDIR_PATH      16  /* with _NAMES: add the path to the filenames */
+#define GETDIR_UNSORTED  32  /* return the results unsorted */
+
+#define GETDIR_ALL        7  /* return names, sizes and dates */
+
+#endif /* LPC_FILES_H_ */
