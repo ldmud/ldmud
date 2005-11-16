c01f27a7320048d81a406f9f13a50ddcf9087ed6
Lars <lars@bearnip.com>
2005-11-16 03:35:37 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/611.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@1989 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 611/mud/lp-245/room/mount_top.c
@@ -0,0 +1,7 @@
+#include "room.h"
+
+TWO_EXIT("room/ravine", "down",
+	 "room/mount_top2", "east",
+	 "Top of mountain",
+	 "You are on top of a mountain. There is a small plateau to the\n"+
+	 "east.\n", 1)
