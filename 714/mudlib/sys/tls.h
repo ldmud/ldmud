0a82a5716e7558efec050e034a4fab1877395a39
Lars <lars@bearnip.com>
2005-11-17 13:48:29 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/714.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@2199 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 714/mudlib/sys/tls.h
@@ -0,0 +1,76 @@
+#ifndef LPC_TLS_H
+#define LPC_TLS_H
+
+/* Field indices for the result of tls_query_connection_info() */
+
+#define TLS_CIPHER  0
+#define TLS_COMP    1
+#define TLS_KX      2
+#define TLS_MAC     3
+#define TLS_PROT    4
+
+#define TLS_INFO_MAX 5  /* Total number of result fields */
+
+/* Interpretation of the cipher information */
+
+#define TLS_CIPHER_TABLE ({ \
+"TLS_CIPHER_NONE", \
+"TLS_CIPHER_NULL", \
+"TLS_CIPHER_ARCFOUR_128", \
+"TLS_CIPHER_3DES_CBC", \
+"TLS_CIPHER_RIJNDAEL_128_CBC", \
+"TLS_CIPHER_RIJNDAEL_256_CBC", \
+"TLS_CIPHER_ARCFOUR_40" \
+})
+
+#define TLS_CIPHER_NAME(x) TLS_CIPHER_TABLE[(x)]
+
+/* Interpretation of the key-exchange information */
+
+#define TLS_KX_TABLE ({ \
+"TLS_KX_NONE", \
+"TLS_KX_RSA", \
+"TLS_KX_DHE_DSS", \
+"TLS_KX_DHE_RSA", \
+"TLS_KX_ANON_DH", \
+"TLS_KX_SRP", \
+"TLS_KX_RSA_EXPORT", \
+"TLS_KX_SRP_RSA", \
+"TLS_KX_SRP_DSS" \
+})
+
+#define TLS_KX_NAME(x) TLS_KX_TABLE[(x)]
+
+/* Interpretation of the MAC information */
+
+#define TLS_MAC_TABLE ({ \
+"TLS_MAC_NONE", \
+"TLS_MAC_NULL", \
+"TLS_MAC_MD5", \
+"TLS_MAC_SHA" \
+})
+
+#define TLS_MAC_NAME(x) TLS_MAC_TABLE[(x)]
+
+/* Interpretation of the compression information */
+
+#define TLS_COMP_TABLE ({ \
+"TLS_COMP_NONE", \
+"TLS_COMP_NULL", \
+"TLS_COMP_ZLIB", \
+"TLS_COMP_LZO" \
+})
+
+#define TLS_COMP_NAME(x) TLS_COMP_TABLE[(x)]
+
+/* Interpretation of the protocol information */
+
+#define TLS_PROT_TABLE ({ \
+"TLS_PROT_NONE", \
+"TLS_PROT_SSL3", \
+"TLS_PROT_TLS1" \
+})
+
+#define TLS_PROT_NAME(x) TLS_PROT_TABLE[(x)]
+
+#endif /* LPC_TLS_H */
