8253965d50f8c2390f078189840c8a736d909f84
Lars <lars@bearnip.com>
2005-11-17 04:10:33 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/673.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@2116 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 673/mud/lp-245/room/mount_top2.c
@@ -0,0 +1,9 @@
+#include "room.h"
+
+ONE_EXIT("room/mount_top", "west",
+	 "Plateau",
+	 "You are on a large, open plateau on top of the mountain.\n"+
+   "The view is fantastic in all directions and the clouds\n"+
+   "that rush past above feels so close you could almost\n"+
+   "touch them. The air here is fresh and clean.\n",1)
+
