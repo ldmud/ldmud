5852c1eaf4185613d81d34bf883aa89c76f0576c
Lars <lars@bearnip.com>
2005-11-17 04:47:39 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/688.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@2147 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 688/mud/lp-245/room/mine/tunnel18.c
@@ -0,0 +1,4 @@
+#include "../room.h"
+ONE_EXIT("room/mine/tunnel17", "east",
+	 "Dead end",
+	 "In the tunnel into the mines.\n", 0)
---
+++ 688/mud/lp-245/room/plane2.c
@@ -0,0 +1,8 @@
+#include "room.h"
+
+FOUR_EXIT("room/plane1", "south",
+	  "room/plane3", "north",
+	  "room/plane4", "east",
+	  "room/plane5", "west",
+	  "A large open plain",
+	  "A large open plain, extending in all directions.\n", 1)
