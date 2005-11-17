97012667d0d1f79b17f8770ba665a8161307a598
Lars <lars@bearnip.com>
2005-11-17 05:56:13 +0000

    Tag branch/prcs-import-32-588 as version/3.2-dev/705.
    git-svn-id: svn://svn.bearnip.com/ldmud/version/3.2-dev@2181 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

---
+++ 705/mud/lp-245/room/crop.c
@@ -0,0 +1,6 @@
+#include "room.h"
+ONE_EXIT("room/vill_shore","north",
+"Fields",
+"You are in the middle of the fields where the city grows all its crops.\n"+
+"A road runs north of here.\n",
+1)
---
+++ 705/mud/lp-245/room/vill_shore2.c
@@ -0,0 +1,6 @@
+#include "room.h"
+TWO_EXIT("room/jetty","west",
+	 "room/jetty2","east",
+	 "Village shore",
+"The village shore. A jetty leads out to the east. To the north some stairs\n"+
+"leads down to the north beach. A road starts to the west\n", 1)
