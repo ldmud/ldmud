622cddf225617214fb28cf5526498de4a43e5471
Lars <lars@bearnip.com>
2005-11-25 05:41:36 +0000

    - (simulate.c) Errors during the processing of a fatal() will be       recognized and treated as double-fatal (Bug #408).
    git-svn-id: svn://svn.bearnip.com/ldmud/3.2/trunk@2230 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/simulate.c
+++ src/simulate.c
@@ -287,6 +287,10 @@ Bool master_will_be_updated = MY_FALSE;
   /* TRUE if a master-update was requested.
    */
 
+static Bool in_fatal = MY_FALSE;
+  /* TRUE if fatal() is being processed.
+   */
+
 int num_error = 0;
   /* Number of recursive calls to errorf().
    */
@@ -604,7 +608,6 @@ fatal (char *fmt, ...)
 {
     va_list va;
     char *ts;
-    static Bool in_fatal = MY_FALSE;
 
     /* Prevent double fatal. */
     if (in_fatal)
@@ -736,6 +739,12 @@ errorf (char *fmt, ...)
     char      fixed_fmt[ERROR_FMT_LEN];
     mp_int    line_number = 0;
     va_list   va;
+
+    /* Errors during the fatal() processing will abort the process
+     * immediately.
+     */
+    if (in_fatal)
+        fatal("Error during fatal().");
 
     ts = time_stamp();
 
