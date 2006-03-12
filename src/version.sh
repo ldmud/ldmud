9765a62cd84425d93518109952be9f03850bf621
Lars <lars@bearnip.com>
2006-03-12 20:55:47 +0000

    New version 3.3.713.
    git-svn-id: svn://svn.bearnip.com/ldmud/trunk@2281 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/version.sh
+++ src/version.sh
@@ -17,9 +17,9 @@ version_longtype="development"
 # A timestamp, to be used by bumpversion and other scripts.
 # It can be used, for example, to 'touch' this file on every build, thus
 # forcing revision control systems to add it on every checkin automatically.
-version_stamp="Sun Mar 12 00:20:07 MST 2006"
+version_stamp="2006-03-12 13:52:44"
 
 # The version number information
-version_micro=712
+version_micro=713
 version_minor=3
 version_major=3
