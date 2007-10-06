6feccf4d87a7d8f67788ef69d4584f6937f72505
Lars <lars@bearnip.com>
2007-10-06 23:55:23 +0000

    - (configure) gentoo Linux requires the '-n' argument for 'tail +2' to        work correctly (thanks, Juanjo!).
    git-svn-id: svn://svn.bearnip.com/ldmud/3.2/trunk@2330 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/version.sh
+++ src/version.sh
@@ -17,7 +17,7 @@ version_longtype="development"
 # A timestamp, to be used by bumpversion and other scripts.
 # It can be used, for example, to 'touch' this file on every build, thus
 # forcing revision control systems to add it on every checkin automatically.
-version_stamp="Sat Oct  6 10:04:57 MDT 2007"
+version_stamp="Sat Oct  6 10:38:52 MDT 2007"
 
 # The version number information
 version_micro=15
