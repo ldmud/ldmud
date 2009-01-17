cdf3ffc92e8fffcecb17bff84d9cd4684aa70b15
Zesstra <zesstra@zesstra.de>
2009-01-17 15:53:33 +0000

    - (hash.h)   Added gcc style attributes to function prototypes.
    git-svn-id: svn://svn.bearnip.com/ldmud/trunk@2504 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/hash.h
+++ src/hash.h
@@ -10,9 +10,13 @@ typedef unsigned char  chash_t;
 #define MAX_WHASH (USHRT_MAX)
 #define MAX_CHASH (UCHAR_MAX)
 
-extern whash_t whashmem (const char *s, size_t len, int maxn);
-extern whash_t whashmem2 (const char *s, size_t len, int maxn, whash_t initial);
-extern whash_t whashstr (const char *s, int maxn);
-extern chash_t chashstr (const char *s, int maxn);
+extern whash_t whashmem (const char *s, size_t len, int maxn)
+                         __attribute__((nonnull(1))) __attribute__((pure));
+extern whash_t whashmem2 (const char *s, size_t len, int maxn, whash_t initial)
+                         __attribute__((nonnull(1))) __attribute__((pure));
+extern whash_t whashstr (const char *s, int maxn)
+                         __attribute__((nonnull(1))) __attribute__((pure));
+extern chash_t chashstr (const char *s, int maxn)
+                         __attribute__((nonnull(1))) __attribute__((pure));
 
 #endif /* HASH_H__ */
