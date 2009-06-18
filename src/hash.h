45f673db9e77f2ab3386bef58d0519a50d53818a
Zesstra <zesstra@zesstra.de>
2009-06-18 21:29:30 +0000

    Replace the 3 hashes in hash.c by Bob Jenkins lookup3 hash.
    hash.c contains now two hash functions, one specialised for little
    endian, the other specialised for big endian machines. The right one for
    the machine is exported by hashmem32() and hashmem32_chained().
    Additionally, three sizes of hash values are available: hash8_t, hash16_t
    and hash32_t.
    (hash.c, hash.h)
    
    git-svn-id: svn://svn.bearnip.com/ldmud/trunk@2676 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/hash.h
+++ src/hash.h
@@ -5,28 +5,53 @@
 #include <limits.h>
 #include "driver.h"
 
-typedef uint8_t   chash_t;
-typedef uint16_t  whash_t;
-typedef uint32_t  dwhash_t;
+// seems a bit superfluous, bit I think, the semantics of the type is clearer.
+typedef uint8_t   hash8_t;
+typedef uint16_t  hash16_t;
+typedef uint32_t  hash32_t;
 
-#define MAX_CHASH  (UINT8_MAX)
-#define MAX_WHASH  (UINT16_MAX)
-#define MAX_DWHASH (UINT32_MAX)
+#define MAX_HASH8  (UINT8_MAX)
+#define MAX_HASH16 (UINT16_MAX)
+#define MAX_HASH32 (UINT32_MAX)
 
-#define INITIAL_DWHASH 5381
+#define INITIAL_HASH 5381
 
-extern whash_t whashmem (const char *s, size_t len, int maxn)
-                         __attribute__((nonnull(1))) __attribute__((pure));
-extern whash_t whashmem2 (const char *s, size_t len, int maxn, whash_t initial)
-                         __attribute__((nonnull(1))) __attribute__((pure));
-extern whash_t whashstr (const char *s, int maxn)
-                         __attribute__((nonnull(1))) __attribute__((pure));
-extern chash_t chashstr (const char *s, int maxn)
-                         __attribute__((nonnull(1))) __attribute__((pure));
+#define hashsize(n) (((uint32_t)1<<(n))-1)
+#define hashmask(n) (hashsize(n) - 1)
 
-extern dwhash_t dwhashmem2 (const char *s, size_t len, int maxn, dwhash_t initial);
-extern dwhash_t dwhashstr (const char *s, int maxn);
+#ifdef WORDS_BIGENDIAN
+extern uint32_t hashbig( const void *key, size_t length, uint32_t initval);
+#else
+extern uint32_t hashlittle( const void *key, size_t length, uint32_t initval);
+#endif
 
-#define dwhashmem(pTxt,len,maxn)   dwhashmem2(pTxt, len, maxn, INITIAL_DWHASH)
+// Hash <len> characters into a uint32_t integer and return the result.
+static INLINE hash32_t hashmem32(const char *s, size_t len)
+                        __attribute__((nonnull(1))) __attribute__((pure))
+                        __attribute__((flatten));
+
+// Hash <len>characters into a hash32_t. Uses <initval> as initial hash value 
+// and is used to hash several strings into one hash value.
+static INLINE hash32_t hashmem32_chained(const char *s, size_t len, hash32_t initval)
+                        __attribute__((nonnull(1))) __attribute__((pure))
+                        __attribute__((flatten));
+
+static INLINE hash32_t hashmem32(const char *s, size_t len)
+{
+#ifdef WORDS_BIGENDIAN
+    return hashbig(s, len, INITIAL_HASH);
+#else
+    return hashlittle(s, len, INITIAL_HASH);
+#endif
+}
+
+static INLINE hash32_t hashmem32_chained(const char *s, size_t len, hash32_t initval)
+{
+#ifdef WORDS_BIGENDIAN
+    return hashbig(s, len, initval);
+#else
+    return hashlittle(s, len, initval);
+#endif
+}
 
 #endif /* HASH_H__ */
