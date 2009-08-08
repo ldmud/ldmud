748ef4e65d7d3d355551b75c56c868a0d5cc285c
Gnomi <Gnomi@UNItopia.rus.Uni-Stuttgart.DE>
2009-08-08 20:41:05 +0000

    Added leading zeroes to date.
    git-svn-id: svn://svn.bearnip.com/ldmud/3.2/trunk@2710 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/version.sh
+++ src/version.sh
@@ -17,7 +17,7 @@ version_longtype="development"
 # A timestamp, to be used by bumpversion and other scripts.
 # It can be used, for example, to 'touch' this file on every build, thus
 # forcing revision control systems to add it on every checkin automatically.
-version_stamp="Sat Aug  8 22:30:00 CEST 2009"
+version_stamp="Sat Aug 08 22:30:00 CEST 2009"
 
 # The version number information
 version_micro=16
