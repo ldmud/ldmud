835123e06af445049f8434068daa964fe55a1dcc
Zesstra <zesstra@zesstra.de>
2009-09-13 14:38:17 +0000

    Replaced Bob Jenkins Hash3 by Murmurhash2.
    During tests with strings from MorgenGrauen and Avalon the Murmurhash2
    performed just as well as Jenkins Hash (maybe even a bit better), but the
    hashing speed was nearly doubled.
    
    git-svn-id: svn://svn.bearnip.com/ldmud/trunk@2741 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/hash.h
+++ src/hash.h
@@ -2,10 +2,11 @@
 #define HASH_H__ 1
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <limits.h>
 #include "driver.h"
 
-// seems a bit superfluous, bit I think, the semantics of the type is clearer.
+// seems a bit superfluous, but I think, the semantics of the type is clearer.
 typedef uint8_t   hash8_t;
 typedef uint16_t  hash16_t;
 typedef uint32_t  hash32_t;
@@ -19,39 +20,31 @@ typedef uint32_t  hash32_t;
 #define hashsize(n) (((uint32_t)1<<(n))-1)
 #define hashmask(n) (hashsize(n) - 1)
 
-#ifdef WORDS_BIGENDIAN
-extern uint32_t hashbig( const void *key, size_t length, uint32_t initval);
-#else
-extern uint32_t hashlittle( const void *key, size_t length, uint32_t initval);
-#endif
+extern uint32_t MurmurHash2 (void const * const key, size_t len, uint32_t seed);
 
 // Hash <len> characters into a uint32_t integer and return the result.
-static INLINE hash32_t hashmem32(const char *s, size_t len)
+static INLINE hash32_t hashmem32(void const * key, size_t len)
                         __attribute__((nonnull(1))) __attribute__((pure))
                         __attribute__((flatten));
 
-// Hash <len>characters into a hash32_t. Uses <initval> as initial hash value 
+// Hash <len> characters into a hash32_t. Uses <initval> as initial hash value 
 // and is used to hash several strings into one hash value.
-static INLINE hash32_t hashmem32_chained(const char *s, size_t len, hash32_t initval)
+static INLINE hash32_t hashmem32_chained(void const * key, size_t len, hash32_t initval)
                         __attribute__((nonnull(1))) __attribute__((pure))
                         __attribute__((flatten));
 
-static INLINE hash32_t hashmem32(const char *s, size_t len)
+
+
+static INLINE hash32_t hashmem32(void const * key, size_t len)
 {
-#ifdef WORDS_BIGENDIAN
-    return hashbig(s, len, INITIAL_HASH);
-#else
-    return hashlittle(s, len, INITIAL_HASH);
-#endif
+    return MurmurHash2(key, len, INITIAL_HASH);
 }
 
-static INLINE hash32_t hashmem32_chained(const char *s, size_t len, hash32_t initval)
+static INLINE hash32_t hashmem32_chained(void const * key, size_t len, hash32_t initval)
 {
-#ifdef WORDS_BIGENDIAN
-    return hashbig(s, len, initval);
-#else
-    return hashlittle(s, len, initval);
-#endif
+    return MurmurHash2(key, len, initval);
 }
+
+
 
 #endif /* HASH_H__ */
