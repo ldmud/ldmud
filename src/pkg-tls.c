ebedce64be38b5b2fa6e9a70ddd5c67ea1acc4ca
Zesstra <zesstra@zesstra.de>
2009-10-05 20:59:51 +0000

    Don't implicitly free the client CA list (context->client_CA).
    Because we set the old CA list again with SSL_CTX_set_client_CA_list(),
    and this function implicitly free's the old list, the memory of the
    list got overwritten and this lead to a crash in TLS upon
    tls_init_connection(). (#677)
    (pkg-tls.c)
    
    git-svn-id: svn://svn.bearnip.com/ldmud/3.2/trunk@2755 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/pkg-tls.c
+++ src/pkg-tls.c
@@ -407,20 +407,22 @@ tls_verify_init (void)
     }
     if (trustfile != NULL)
     {
-	stack = SSL_load_client_CA_file(trustfile);
+        stack = SSL_load_client_CA_file(trustfile);
+        SSL_CTX_set_client_CA_list(context, stack);
     }
     else
     {
-	stack = SSL_CTX_get_client_CA_list(context);
-    }
-    if (trustdirectory != NULL)
-    {
-	SSL_add_dir_cert_subjects_to_stack(stack, trustdirectory);
+        stack = SSL_CTX_get_client_CA_list(context);
+        if (stack == NULL)
+        {
+            stack = sk_X509_NAME_new_null();
+            SSL_CTX_set_client_CA_list(context, stack);
+        }
     }
 
-    if (stack != NULL)
+    if (trustdirectory != NULL && stack != NULL)
     {
-	SSL_CTX_set_client_CA_list(context, stack);
+        SSL_add_dir_cert_subjects_to_stack(stack, trustdirectory);
     }
 }
 #endif
