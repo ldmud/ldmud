8b21f449839aa8de02f5b8d2b5ccbbbb61214f21
Zesstra <zesstra@zesstra.de>
2011-01-10 21:59:02 +0000

    Added some function attributes and assert().
    Some functions should never get NULL pointers as args.
    Lengths of 0 in SSL_write() lead to undefined behaviour. Should never
    happen and will be asserted now.
    (pkg-openssl.c)
    
    git-svn-id: svn://svn.bearnip.com/ldmud/trunk@2962 56160b6e-f8f8-0310-bd54-bccebfe6a2f2

--- src/pkg-openssl.c
+++ src/pkg-openssl.c
@@ -9,6 +9,7 @@
 #if defined(USE_TLS) && defined(HAS_OPENSSL)
 
 #include <stdio.h>
+#include <assert.h>
 
 #include <openssl/ssl.h>
 #include <openssl/rand.h>
@@ -73,6 +74,8 @@ openssl_malloc (size_t size)
 }
 
 static void
+openssl_free (void * ptr) __attribute__((nonnull));
+static void
 openssl_free (void * ptr)
 /*
  * Wrapper function for using our own allocator in openssl.
@@ -81,6 +84,8 @@ openssl_free (void * ptr)
     pfree(ptr);
 }
 
+static void *
+openssl_realloc (void * ptr, size_t size) __attribute__((nonnull));
 static void *
 openssl_realloc (void * ptr, size_t size)
 /*
@@ -139,6 +144,8 @@ set_dhe1024 (void)
 } /* set_dhe1024() */
 
 /*-------------------------------------------------------------------------*/
+static int
+tls_verify_callback(int preverify_ok, X509_STORE_CTX *ctx) __attribute__((nonnull));
 static int
 tls_verify_callback(int preverify_ok, X509_STORE_CTX *ctx) 
 
@@ -502,6 +509,8 @@ dump_openssl_error_stack()
 
 /*-------------------------------------------------------------------------*/
 int
+tls_read (interactive_t *ip, char *buffer, int length) __attribute__((nonnull));
+int
 tls_read (interactive_t *ip, char *buffer, int length)
 
 /* Read up to <length> bytes data for the TLS connection of <ip>
@@ -557,6 +566,8 @@ tls_read (interactive_t *ip, char *buffer, int length)
 
 /*-------------------------------------------------------------------------*/
 int
+tls_write (interactive_t *ip, char *buffer, int length) __attribute__((nonnull));
+int
 tls_write (interactive_t *ip, char *buffer, int length)
 
 /* Write <length> bytes from <buffer> to the TLS connection of <ip>
@@ -565,6 +576,10 @@ tls_write (interactive_t *ip, char *buffer, int length)
  */
 
 {
+#ifdef DEBUG
+    // should never happen since OpenSSL's behaviour is undefined for length==0
+    assert(length > 0);
+#endif
     int ret = SSL_write(ip->tls_session, buffer, length);
 
     if (ret <= 0)
@@ -589,6 +604,8 @@ tls_write (interactive_t *ip, char *buffer, int length)
 } /* tls_write() */
 
 /*-------------------------------------------------------------------------*/
+int
+tls_do_handshake (interactive_t *ip) __attribute__((nonnull));
 int
 tls_do_handshake (interactive_t *ip)
 
@@ -634,6 +651,8 @@ tls_do_handshake (interactive_t *ip)
 
 /*-------------------------------------------------------------------------*/
 int
+tls_init_connection (interactive_t *ip) __attribute__((nonnull));
+int
 tls_init_connection (interactive_t *ip)
 
 /* Starts a TLS secured connection to the interactive <ip>.
@@ -670,6 +689,8 @@ tls_init_connection (interactive_t *ip)
 } /* tls_init_connection() */
 
 /*-------------------------------------------------------------------------*/
+vector_t *
+tls_check_certificate (interactive_t *ip, Bool more) __attribute__((nonnull));
 vector_t *
 tls_check_certificate (interactive_t *ip, Bool more)
 
@@ -823,6 +844,8 @@ tls_check_certificate (interactive_t *ip, Bool more)
 
 /*-------------------------------------------------------------------------*/
 void
+tls_deinit_connection (interactive_t *ip) __attribute__((nonnull));
+void
 tls_deinit_connection (interactive_t *ip)
 
 /* Close the TLS connection for the interactive <ip> if there is one.
@@ -858,6 +881,8 @@ tls_error (int err)
 } /* tls_error() */
 
 /*-------------------------------------------------------------------------*/
+vector_t *
+tls_query_connection_info (interactive_t *ip) __attribute__((nonnull));
 vector_t *
 tls_query_connection_info (interactive_t *ip)
 
@@ -898,6 +923,8 @@ tls_available ()
  * Interface to the openssl cryptography api
  *------------------------------------------------------------------
  */
+Bool
+get_digest (int num, digest_t * md, size_t *len) __attribute__((nonnull));
 Bool
 get_digest (int num, digest_t * md, size_t *len)
 
@@ -971,6 +998,9 @@ get_digest (int num, digest_t * md, size_t *len)
 } /* get_digest() */
 
 /*-------------------------------------------------------------------------*/
+void
+calc_digest (digest_t md, void *dest, size_t destlen, void *msg, size_t msglen, void *key, size_t keylen)
+             __attribute__((nonnull(2,4)));
 void
 calc_digest (digest_t md, void *dest, size_t destlen, void *msg, size_t msglen, void *key, size_t keylen)
 
