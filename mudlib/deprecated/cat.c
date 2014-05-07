f37aaede62bc50ec700c942143b913b49d37f1d0
Zesstra <zesstra@zesstra.de>
2014-05-07 22:02:11 +0200

    sefun cat(): added efun:: qualifiers.
    Forced calling the real efuns in case a mud has sefuns shadowing
    the efuns needed in the cat() implementation. Calling them with
    set_this_object() in effect is not possible.

--- mudlib/deprecated/cat.c
+++ mudlib/deprecated/cat.c
@@ -1,17 +1,23 @@
 /* This sefun is meant to replace the deprecated efun cat().
  * Feel free to add it to your mudlibs, if you have much code using cat() or
  * want to use cat().
+ *
+ * NOTE: the efun:: qualifiers are there in case you have sefuns shadowing
+ *       these efuns. They can't be called with set_this_object() in effect.
+ *       If you need to call your sefuns, please move the plain calls to
+ *       read_file() with the set_this_object() into a separate private
+ *       function.
  */
 #define CAT_MAX_LINES 50
 
 varargs int cat(string file, int start, int num)
 {
     if (extern_call())
-        set_this_object(previous_object());
+        efun::set_this_object(efun::previous_object());
 
     int more;
 
-    if (num < 0 || !this_player())
+    if (num < 0 || !efun::this_player())
         return 0;
 
     if (!start)
@@ -19,18 +25,18 @@ varargs int cat(string file, int start, int num)
 
     if (!num || num > CAT_MAX_LINES) {
         num = CAT_MAX_LINES;
-        more = sizeof(read_file(file, start+num, 1));
+        more = efun::sizeof(efun::read_file(file, start+num, 1));
     }
 
-    string txt = read_file(file, start, num);
+    string txt = efun::read_file(file, start, num);
     if (!txt)
         return 0;
 
-    tell_object(this_player(), txt);
+    efun::tell_object(efun::this_player(), txt);
 
     if (more)
-        tell_object(this_player(), "*****TRUNCATED****\n");
+        efun::tell_object(efun::this_player(), "*****TRUNCATED****\n");
 
-    return sizeof(txt & "\n");
+    return efun::sizeof(txt & "\n");
 }
 
