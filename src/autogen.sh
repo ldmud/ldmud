81e305855d98a25a4fbb00e987c6e0af3fd5f179
Karl Tiedt <ktiedt@gmail.com>
2015-07-19 02:07:19 -0700

    Cleanup of pre-compiled autoconf config, this should allow for a more clean git status when actively building

--- src/autogen.sh
+++ src/autogen.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 # Skeleton script for auto-generating some files like configure for compiling
-# the driver.
+# the driver. Which will now check if autoconf is installed first if not, it
+# will copy the premade packaged files into place.
 
-cd autoconf
+if command -v autoconf >/dev/null; then
+	cd autoconf
 
-autoreconf
+	autoreconf
 
-mv configure ..
-mv machine.h.in ..
+	mv configure ..
+	mv machine.h.in ..
 
-cd ..
-
+	cd ..	
+else
+	echo "Autoconf not found: copying premade configure and machine.h.in files from ../autoconf"
+	cp ../autoconf/configure .
+	cp ../autoconf/machine.h.in .
+fi
